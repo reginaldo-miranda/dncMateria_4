93548e975c23739513227ca8ad6aa31ad51b6ffa
reginaldo miranda <reginaldomiranda@MacBook-Pro-de-reginaldo.local>
2025-03-10 19:55:03 -0300

    header

--- dncMateria4/src/components/header/Header.jsx
+++ dncMateria4/src/components/header/Header.jsx
@@ -1,15 +1,28 @@
 import {Link} from 'react-router-dom';
 
 //assets
-import logo from '../../assets/dnc-logo 2.svg';
+import './header.css'
+import logo from '../../assets/dnc-logo.svg'
 
 function Header(){
     return (
-        <div className="header">
-            <div className="container al-center d-flex jc-space-between" >
+        <header>
+              <div className="container">
+                <div className="al-center d-flex jc-space-between">
                 <Link to="/"><img src={logo} /></Link>
+                <nav>
+                    <ul className="d-flex">
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/about">About</Link></li>
+                        <li><Link to="/projects">Projects</Link></li>
+                        <li><Link to="/contact">Contact</Link></li>
+                    </ul>
+                </nav>
             </div>
         </div>
+
+        </header>
+      
     )
 }
 export default Header;  //export the component
