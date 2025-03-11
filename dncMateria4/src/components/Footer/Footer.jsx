cea28078fac59979bc979b3aae369c5bc9c1d612
reginaldo-miranda <50125952+reginaldo-miranda@users.noreply.github.com>
2025-03-11 18:16:17 +0000

    footer ruim

--- dncMateria4/src/components/Footer/Footer.jsx
+++ dncMateria4/src/components/Footer/Footer.jsx
@@ -18,8 +18,9 @@ function Footer(){
                 <div className="d-flex jc-space-between">
                     <div className="footer-logo-col">
                         <img src={logo} alt="Logo"/>
-                        <p className="grey-1-color">A escola que prepara você para as profissões em alta no mercado de trabalho.</p>
-                        <div className="d-flex social-link">
+                        <p className="grey-1-color">A escola que prepara você para as profissões em <br>
+                        </br> alta no mercado de trabalho.</p>
+                        <div className=" social-links">
 
                              <a href="https://www.facebook.com/" target="_blank" >
                                  <img src={facebook}/>
@@ -37,24 +38,30 @@ function Footer(){
                         </div>
                      
                     </div>
-                    <div className='d-flex'>
-                           <div className='footer-col' >
+                    <div className ='d-flex'>
+                           <div className="footer-col">
                             <h3>Pages</h3>
-                                 <ul className="d-flex">
+                                 <ul>
                                       <li><Link to="/">Home</Link></li>
                                       <li><Link to="/about">About</Link></li>
-                                       <li><Link to="/projects">Projects</Link></li>
-                                     <li><Link to="/contact">Contact</Link></li>
+                                      <li><Link to="/projects">Projects</Link></li>
+                                      <li><Link to="/contact">Contact</Link></li>
                                   </ul>
                            </div>
+                                <div className='footer-col'>
+                                    <h3>Contact</h3>
+                                    <p className='grey-1-color'>R. Justino Cobra, 61 – Vila Ema | São José dos Campos – SP | CEP 12243-030</p> 
+                                    <p className='grey-1-color'>suporte@escoladnc.com.br</p>
+                                    <p className='grey-1-color'>(19) 99187-4342</p>
+                                </div>
                         </div>
                    
                 </div>
                 <div className='d-flex jc-space-between footer-copy'>
                     <p className='grey-1-color'>Copyright © DNC - 2024</p>
                     <div className='langs-area d-flex'>
-                        <img src={brazilLogo} />
-                        <img src={usaLogo} />
+                        <img src={brazilLogo} height="29px" />
+                        <img src={usaLogo} height="29px" />
                     </div>
                 </div>
                 
