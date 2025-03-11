bf3c37709feafcc6a7e0c1c8d87cc773197025da
reginaldo-miranda <50125952+reginaldo-miranda@users.noreply.github.com>
2025-03-11 20:23:08 +0000

    componente button

---
+++ dncMateria4/src/components/Button/Button.jsx
@@ -0,0 +1,13 @@
+
+
+// assets
+import './Button.css'
+function Button(arrow, buttonStyle, loading, children, ...props  ){
+    return (
+        <button className={`button ${buttonStyle}`}>
+            {children}
+        </button>
+    )
+}
+
+export default Button
