1f8f4570c835cfb1d2ad82d7ad76418c75da59b4
reginaldo miranda <reginaldobrain@gmail.coma>
2025-03-16 17:04:48 -0300

    inseriu usestate

--- dncMateria4/src/components/header/Header.jsx
+++ dncMateria4/src/components/header/Header.jsx
@@ -1,16 +1,33 @@
+import { useState } from 'react';
 import {Link} from 'react-router-dom';
 
 //assets
 import './Header.css'
 import logo from '../../assets/dnc-logo.svg'
 
+//components
+import Button from '../Button/Button';
+
 function Header(){
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const toggleMenu = () =>{
+        setIsMenuOpen(!isMenuOpen);
+    }
     return (
         <header>
               <div className="container">
                 <div className="al-center d-flex jc-space-between">
                 <Link to="/"><img src={logo} /></Link>
-                <nav>
+                <div className="mobile-menu">
+                    <Button buttonStyle="secundary"  onClick={toggleMenu}>
+                        Menu
+                    </Button>
+                      
+                </div>
+                <nav className={`${isMenuOpen ? 'open' : ''}`}>
+                    <Button buttonStyle="unstyled" className="mobile-menu close-btn" onClick={toggleMenu}>
+                        X
+                    </Button>
                     <ul className="d-flex">
                         <li><Link to="/">Home</Link></li>
                         <li><Link to="/about">About</Link></li>
