a0a3facae62d14696446a496c5bb27d29d528862
reginaldo miranda <reginaldobrain@gmail.coma>
2025-03-16 17:50:53 -0300

    useEffect

--- dncMateria4/src/App.jsx
+++ dncMateria4/src/App.jsx
@@ -7,10 +7,15 @@ import About from './pages/About'
 import Contact from './pages/Contact'
 import Projects from './pages/Projects';
 
+//utils
+
+import ScrollToTop from './utilis/ScrollTop';
+
 function App() {
 
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
