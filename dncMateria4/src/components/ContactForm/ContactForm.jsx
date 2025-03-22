39fed06d841ac3f1cbd487b1d61eafdbf88b8c73
reginaldo miranda <reginaldobrain@gmail.coma>
2025-03-21 22:18:14 -0300

    text area

--- dncMateria4/src/components/ContactForm/ContactForm.jsx
+++ dncMateria4/src/components/ContactForm/ContactForm.jsx
@@ -9,25 +9,29 @@ function ContactForm(){
        
            
               <div className="contact-form d-flex fd-column al-center">
-                    <h2>We love meeting new people and helping then.</h2>
+                    <h2>We love meeting new people and helping them.</h2>
                      <form>
-                            <div className='d-felx form-group'>
+                            <div className='d-flex form-group'>
                                    <input className="form-input" type='text'  id="name" name="name" placeholder='Name *'/>
-                                   <input className="form-input" type='email' id="email" name="email" placeholder='email *'/>
-                                   <textarea className="form-input" id="message" name="message" placeholder='Message *' rows="4">
 
-                                   </textarea>
-                                   <div>
-                                          <Button type='submit' buttonStyle="primary">
-                                               Enviar
-                                          </Button>      
-                                   </div>
+                                   <input className="form-input" type='email' id="email" name="email" placeholder='email *'/>
+
+                                 
                             </div>
+                            <div className="d-flex form-group">
+                                   <textarea className="form-input" id="message" name="message" placeholder='Message *' rows="4"
+                                   />
+                                   
+                            </div>       
+
+                            <div className='al-center d-flex jc-end form-group'>
+                                <Button type='submit' buttonStyle="secundary">
+                                   Enviar
+                                </Button>      
+                            </div>      
                      </form>
               </div>
                
-           
-        
     )
 }
 export default ContactForm;
