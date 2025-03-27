821270c374f079da68accc80fb35cb581d53bec9
reginaldo miranda <reginaldobrain@gmail.coma>
2025-03-27 16:49:08 -0300

    feito storange

--- dncMateria4/src/contexts/AppContext.jsx
+++ dncMateria4/src/contexts/AppContext.jsx
@@ -4,7 +4,9 @@ import {getApiData} from "../services/apiServices"
 export const AppContext = createContext()
 export const AppProvider = ({children}) => {
 
-    const [language, setLanguage] = useState('br')
+    const saveLanguage = localStorage.getItem('lang')
+
+    const [language, setLanguage] = useState(saveLanguage ?? 'br')
     const [languages, setLanguages] = useState()
     const [loading, setLoading] = useState(true)
 
@@ -22,6 +24,11 @@ export const AppProvider = ({children}) => {
         }
         fetcLanguages()
     }, [])
+
+    useEffect(() => {
+        localStorage.setItem('lang', language)
+    },[language])
+
     return (
         <AppContext.Provider value={{language, languages, setLanguage, loading }}>
             {children}
