07505ba1040bfb5dba273401f3c78e1948786a54
reginaldo miranda <reginaldobrain@gmail.coma>
2025-03-28 10:54:04 -0300

    session

--- dncMateria4/src/components/ProjectsList/ProjectsList.jsx
+++ dncMateria4/src/components/ProjectsList/ProjectsList.jsx
@@ -1,3 +1,4 @@
+import { useContext } from 'react';
 import { useState, useEffect } from 'react'
 import './ProjectsList.css'
 
@@ -6,12 +7,38 @@ import './ProjectsList.css'
 import likedFilled from '../../assets/like_ok.svg'
 import like from '../../assets/likeNaoGostei.svg'
 
+//components
+import Button from '../Button/Button';
+
 //utilis
 import { getApiData } from '../../services/apiServices'
 import Projects from '../../pages/Projects.jsx'
 
+//context
+import { AppContext } from '../../contexts/AppContext';
+
 function projectsList(){
+    const appContext = useContext(AppContext)
     const [projects, setProjects] = useState([])
+    const [favProjects , setFavProjects] = useState([])
+
+    const saveFavProjects = (id) => {
+        setFavProjects((prevFavProjects) => {
+            if(prevFavProjects.includes(id)){
+                const filterArray = prevFavProjects.filter((project) => project !== id)
+                sessionStorage.setItem('favProjects', JSON.stringify(filterArray))
+                return prevFavProjects.filter((projectId) => projectId !== id)
+            } else{
+                sessionStorage.setItem('favProjects', JSON.stringify([...prevFavProjects, id]))
+                return [...prevFavProjects, id]
+            }
+
+        })
+    }
+
+    const changeLanguage = (country) => {
+        appContext.setLanguage(country)
+    }
 
     useEffect(() => {
       const fetchData = async () => {
@@ -26,12 +53,21 @@ function projectsList(){
        fetchData()
     }, [])
 
+    useEffect(() => {
+        const saveFavProjects = JSON.parse( sessionStorage.getItem('favProjects'))
+        if(saveFavProjects) {
+            setFavProjects(saveFavProjects)
+        }
+       
+    }, [])
+
     return (
         <div className="project-section">
 
            <div className='project-hero'>
-              <h2>Follow Our Projects</h2>
-              <p>It is a long established fact that a reader will be distracted by the of readable content of page  lookings at its layouts  points. </p>
+              <h2>{ appContext.languages[appContext.language].projects.title } </h2>
+            
+              <p>{appContext.languages[appContext.language].projects.subtitle } </p>
            </div>
            <div className='project-grid'>
               {
@@ -45,7 +81,11 @@ function projectsList(){
                           <div>
                               <h3>{project.title}</h3>  
                               <p>{project.subtitle}</p>
-                              <img src={likedFilled} height="20px" alt="Like" />
+
+                              <Button buttonStyle="unstyled" onClick={() => saveFavProjects(project.id)}>
+                                    <img src={favProjects.includes(project.id) ? likedFilled : like } height="20px" alt="Like" />
+                              </Button>
+                              
                           </div>  
                       </div>
                      ))
